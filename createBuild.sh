67e036aca4038001f105c330a4a84c1201da600e
demontricerz <raajbala1999@gmail.com>
2022-07-24 23:53:45 +0530

    SMS

--- createBuild.sh
+++ createBuild.sh
@@ -1,11 +1,12 @@
 ember build --environment=production -o ../masterflow-build
 cd ../masterflow-build
 git init
-git add *
-git commit -m "Commit"
 git remote add origin https://github.com/ecappmakers/masterflow-build.git
 git branch -M main
-git pull origin main
+git pull origin main --rebase
+git add *
+git commit -m "Commit"
+git pull origin main --rebase
 git push origin main --force
 cd ../masterflow-dem
 echo "Build Completed"
