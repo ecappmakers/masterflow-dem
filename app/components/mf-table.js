6885515c51cabd69defc076d1f28cc8568be40b2
Vishnuraaj B <38652605+demontricer@users.noreply.github.com>
2022-09-13 07:19:04 +0530

    all changes

---
+++ app/components/mf-table.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tableTitle: 'Hello World',
+    tableHeader: [
+        '1',
+        '2',
+    ],
+    tableData:[
+        {
+            '1':'1',
+            '2':'2',
+        },
+        {
+            '1':'1',
+            '2':'2',
+        },
+        {
+            '1':'1',
+            '2':'2',
+        },
+        {
+            '1':'1',
+            '2':'2',
+        },
+        {
+            '1':'1',
+            '2':'2',
+        },
+        {
+            '1':'1',
+            '2':'2',
+        },
+    ],
+});
